0ccf7427b095a0673aa9ebd8f09c9f772defcc40
Willians Martins de Morais - Instrutor <Willians@classroom.net>
2019-11-30 15:30:36 -0300

    popularCampos

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -19,6 +19,7 @@ jQuery(document).ready(function( $ ) {
       $('.back-to-top').fadeOut('slow');
     }
   });
+  
   $('.back-to-top').click(function(){
     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
     return false;
