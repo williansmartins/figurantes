336af165b79e51ca62d477e1804cc39a62156998
Willians Martins de Morais - Instrutor <Willians@classroom.net>
2019-12-21 10:31:46 -0300

    navegacao tela de perfil

--- frontend/js/app/login-controller.js
+++ frontend/js/app/login-controller.js
@@ -2,15 +2,18 @@ angular
 .module('principal')
 .controller('LoginController', loginController);
 
-function loginController($scope, LoginService ){
+function loginController($scope, $location, LoginService ){
     $scope.model = new Object();
+    $scope.model.email = "maria@teste.com";
+    $scope.model.senha = "secreta1";
 
     $scope.autenticar = function(){
         LoginService.autenticar($scope.model.email, $scope.model.senha).then(
             function(resposta){
                 console.info(resposta.data);
                 if(resposta.data == true){
-                    alert('Deu Certo');
+                    //alert('Deu Certo');
+                    $location.path('/perfil');
                 }else{
                     alert('Deu Erro');
                 }
