78983951b40805aab3df45007fb2ce3e9b4c289c
Willians Martins <professorwillians@gmail.com>
2020-01-03 22:48:23 -0300

    correcao para h2

--- backend/src/main/resources/carga.sql
+++ backend/src/main/resources/carga.sql
@@ -1,23 +1,9 @@
-#figurante
-INSERT INTO `endereco` VALUES (1,'Itapevi','Sao Paulo'),(3,'Osasco','Sao Paulo');
-INSERT INTO `figurante` VALUES (2,1.78,'305.809.118-11','contato@williansmartins.com','branco','M',37,'Willians Martins',80,'secreta',1),(4,1.6,'305.809.118-11','maria@williansmartins.com','india','F',37,'Maria da Penha',65,'nova',3);
+INSERT INTO endereco  VALUES (1,'Itapevi','Sao Paulo');
+INSERT INTO endereco  VALUES (2,'Osasco','Sao Paulo');
+INSERT INTO figurante VALUES (3,1.78,'305.809.118-11','contato@williansmartins.com','branco','M',37,'Willians Martins',80,'secreta',1);
+INSERT INTO figurante VALUES (4,1.6,'305.809.118-11','maria@williansmartins.com','india','F',37,'Maria da Penha',65,'nova',2);
 
-#login
-INSERT INTO `figurantes`.`login`
-(
-`ativo`,
-`email`,
-`nome`,
-`senha`,
-`tipo`)
-VALUES
-(1, 'maria@teste.com', 'Maria Martins', 'secreta1', 'FIGURANTE');
-INSERT INTO `figurantes`.`login`
-(
-`ativo`,
-`email`,
-`nome`,
-`senha`,
-`tipo`)
-VALUES
-(0, 'joao@teste.com', 'Joao Martins', 'secreta2', 'CLIENTE');
+INSERT INTO login (ativo, email, nome, senha, tipo)
+VALUES (1, 'maria@teste.com', 'Maria Martins', 'secreta1', 'FIGURANTE');
+INSERT INTO login (ativo, email, nome, senha, tipo)
+VALUES (0, 'joao@teste.com', 'Joao Martins', 'secreta2', 'CLIENTE');
